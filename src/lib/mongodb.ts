5ff9dd2cbff5c4786cb4a19a0c729cbc5c4c5a65
Ahmed-Mustafa230 <kaleemrazasamo18@gmail.com>
2026-08-14 22:32:29 +0500

    fix: make MongoDB auth connection resilient

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,12 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const options = {};
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+const options = {
+  serverSelectionTimeoutMS: 10000,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+  waitQueueTimeoutMS: 10000,
+};
 
 if (!uri) {
   throw new Error(
@@ -12,19 +14,51 @@ if (!uri) {
   );
 }
 
-if (process.env.NODE_ENV === 'development') {
-  const globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
+type MongoClientPromise = Promise<MongoClient>;
 
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+declare global {
+  var _mongoClientPromise: MongoClientPromise | undefined;
 }
 
-export default clientPromise;
+let clientPromise: MongoClientPromise | null = null;
+
+function createClientPromise(): MongoClientPromise {
+  const client = new MongoClient(uri!, options);
+  const promise = client.connect();
+
+  void promise.catch(() => {
+    if (clientPromise === promise) {
+      clientPromise = null;
+    }
+    if (global._mongoClientPromise === promise) {
+      global._mongoClientPromise = undefined;
+    }
+  });
+
+  return promise;
+}
+
+function getClientPromise(): MongoClientPromise {
+  if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+      global._mongoClientPromise = createClientPromise();
+    }
+    return global._mongoClientPromise;
+  }
+
+  if (!clientPromise) {
+    clientPromise = createClientPromise();
+  }
+  return clientPromise;
+}
+
+const clientPromiseExport = {
+  then<TResult1 = MongoClient, TResult2 = never>(
+    onFulfilled?: ((value: MongoClient) => TResult1 | PromiseLike<TResult1>) | null,
+    onRejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null
+  ): Promise<TResult1 | TResult2> {
+    return getClientPromise().then(onFulfilled, onRejected);
+  },
+} as unknown as MongoClientPromise;
+
+export default clientPromiseExport;
